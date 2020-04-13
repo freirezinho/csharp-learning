495900bfbe354b4a37574bcebf39ffc6897223f8
Saulo Freire <gbsaulo@gmail.com>
2020-04-13 12:51:06 -0300

    Intro to arrays

---
+++ array_sum/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+
+class Program
+{
+  static void Main(string[] args)
+  {
+    int[] arr = new int[10];
+    int sum = 0;
+
+    for (int i = 0; i < arr.Length; i++)
+    {
+      Console.WriteLine("Enter a number:");
+      arr[i] = Convert.ToInt32(Console.ReadLine());
+      sum = sum + arr[i];
+    }
+    for (int i = 0; i < arr.Length; i++)
+    {
+      Console.WriteLine("{0} ", arr[i]);
+
+    }
+    Console.WriteLine("Sum of the array elements is: {0}", sum);
+
+  }
+}
