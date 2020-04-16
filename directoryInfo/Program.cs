a79babc72e75f0c2f3b19ecc81a2b718ee11b2ca
Saulo Freire <gbsaulo@gmail.com>
2020-04-16 15:59:08 -0300

    Directories

---
+++ directoryInfo/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IO;
+
+namespace directoryInfo
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      DirectoryInfo dir = new DirectoryInfo("./");
+      DirectoryInfo[] dirarr = dir.GetDirectories();
+      FileInfo[] arr = dir.GetFiles();
+      Console.WriteLine("-----------DIRECTORIES----------------------");
+      foreach (DirectoryInfo directory in dirarr)
+      {
+        Console.WriteLine("{0}", directory.FullName);
+      }
+      Console.WriteLine("-----------FILES----------------------");
+      foreach (FileInfo file in arr)
+      {
+        Console.WriteLine("{0}----{1}----{2}", file.FullName, file.Length, file.Extension);
+      }
+    }
+  }
+}
