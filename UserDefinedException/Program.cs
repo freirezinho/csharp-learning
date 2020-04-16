fd5a2f45a50f6d8733a3e9d8cd9bc89904708d17
Saulo Freire <gbsaulo@gmail.com>
2020-04-16 19:45:28 -0300

    Multithreading

---
+++ UserDefinedException/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace UserDefinedException
+{
+  class AgeNotAllowedException : ApplicationException
+  {
+    public AgeNotAllowedException(string s) : base(s)
+    {
+
+    }
+  }
+  class Program
+  {
+    public void AcceptAge()
+    {
+      int age;
+      Console.WriteLine("Enter your age");
+      age = Convert.ToInt32(Console.ReadLine());
+      if (age < 20 || age > 40)
+      {
+        AgeNotAllowedException exp = new AgeNotAllowedException("Age between 20 and 40 is allowed");
+        throw exp;
+      }
+      else
+      {
+        Console.WriteLine("Welcome");
+      }
+    }
+    static void Main(string[] args)
+    {
+      Program p = new Program();
+      try
+      {
+        p.AcceptAge();
+      }
+      catch (AgeNotAllowedException obj)
+      {
+        Console.WriteLine(obj.Message);
+      }
+    }
+  }
+}
---
+++ multithread/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Threading;
+
+namespace multithread
+{
+  class Program
+  {
+    enum ThreadStates
+    {
+      Unstarted,
+      Runnable,
+      NonRunnable, // if Sleeping, Waiting or Blocked
+      Dead
+    }
+    public void Display1()
+    {
+      Console.WriteLine("Display 1 started");
+      Monitor.Enter(this);
+      for (int i = 0; i < 30; i++)
+      {
+        Console.WriteLine("Display 1");
+        Thread.Sleep(1000);
+      }
+      Monitor.Exit(this);
+      Console.WriteLine("Display 1 ended.");
+    }
+    public void Display2()
+    {
+      Console.WriteLine("Display 2 started");
+      Monitor.Enter(this);
+      for (int i = 0; i < 30; i++)
+      {
+        Console.WriteLine("Display 2");
+        Thread.Sleep(1000);
+      }
+      Monitor.Exit(this);
+      Console.WriteLine("Display 2 ended.");
+    }
+    static void Main(string[] args)
+    {
+      Program p = new Program();
+      ThreadStart ts1 = new ThreadStart(p.Display1);
+      Thread th1 = new Thread(ts1);
+      ThreadStart ts2 = new ThreadStart(p.Display2);
+      Thread th2 = new Thread(ts2);
+      th1.Priority = ThreadPriority.Lowest;
+      th2.Priority = ThreadPriority.Highest;
+      th1.Start();
+      th2.Start();
+
+      Thread.Sleep(2000);
+      //   Não suportado em .NET Core
+      //   th1.Abort();
+      //   th1.Suspend();
+      //   th1.Resume();
+    }
+  }
+}
